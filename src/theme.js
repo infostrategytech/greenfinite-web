5d404cb137fb35fdaaa0297cf9ec29468c09a781
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2021-11-12 17:43:07 +0100

    update theme

--- src/theme.js
+++ src/theme.js
@@ -4,10 +4,11 @@ import { createTheme } from "@material-ui/core/styles";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#FFF1DA",
+      main: "#3D8754",
+
     },
     secondary: {
-      main: "#3D8754",
+      main: "#FFF1DA",
     },
   },
 });
