5d456d22e8c8a6a6a95f29545f121cf8f9595516
blesseddominic98 <blesseddominic98@gmail.com>
2021-11-17 15:31:19 +0100

    implemented mr.Tobenna's feedback

--- components/Header.js
+++ components/Header.js
@@ -228,7 +228,7 @@ function Header() {
                         >
                           <MenuItem
                             onClick={() => {
-                              handleClose;
+                              handleClose();
                               router.push('/distributors');
                             }}
                           >
