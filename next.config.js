010a7d449d650d00ae869ee16947cd1ce1bd4b2b
kestonorherhe <36920276+kestonorherhe@users.noreply.github.com>
2021-11-22 12:16:45 +0100

    implements webmail redirect

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    async redirects() {
+      return [
+        {
+          source: '/webmail',
+          destination: 'https://istrategytech.com/webmail',
+          permanent: true,
+        },
+      ]
+    },
+  }
