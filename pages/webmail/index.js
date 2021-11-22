8b57a4d0e014af9008e7313c1c7d38bcbd6796b0
Ehi <41729580+AliEhis@users.noreply.github.com>
2021-11-22 13:18:05 +0100

    Update index.js

--- pages/webmail/index.js
+++ pages/webmail/index.js
@@ -1,10 +1,1 @@
-import React, { useEffect } from "react";
 
-function Webmail() {
-  useEffect(() => {
-    window.location.href = "https://istrategytech.com/webmail";
-  }, []);
-  return <div></div>;
-}
-
-export default Webmail;
